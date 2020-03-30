784d48402aa51fe6c46dc86a8fc06719e6eae8d3
hj <137612270@qq.com>
2020-03-30 09:55:23 +0800

    f

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
-/*
+/* 生产环境优化:
 -----------第一节:-------------------
-oneof
+* oneOf:
+    module/rules中的loader只会被处理一边.比如说项目中的css文件,当别cssloader处理完之后,就不需要再往下扫描其他的loader了.
+
+    使用方法就是:module/urles中加oneOf:[],loader都放入oneOf:[]中.
+
+    但是使用oneOf需要注意:不能有2项配置处理处理同一种类型文件.比如说下面的eslint-loader和babel-loader都是对js文件进行处理的,那么会出现一个问题:2个loader只会生效一个.那么怎么办呢?方法:我们需要把eslint-loader提取到oneOf之外即可,就ok了.->因为eslint-loader中有enforce:'pre',会优先执行.然后处理完之后,就会进入oneOf中,oneOf中只会匹配一个.
 
  */
 const {
@@ -37,38 +42,6 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.css$/,
-                use: [...commonCssLoader]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    ...commonCssLoader,
-                    "less-loader"
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif)$/,
-                loader: "url-loader",
-                options: {
-                    limit: 8 * 1024,
-                    esModule: false,
-                    name: "[hash:10].[ext]",
-                    outputPath: "imgs"
-                }
-            },
-            {
-                test: /\.html$/,
-                loader: "html-loader"
-            },
-            {
-                exclude: /\.(css|js|html|less|jpg|png|gif)$/,
-                loader: "file-loader",
-                options: {
-                    name: "[hash:10].[ext]",
-                    outputPath: "media"
-                }
-            },
             // { //eslint语法检查
             //   test: /\.js$/,
             //   exclude: /node_modules/,
@@ -78,35 +51,70 @@ module.exports = {
             //     fix: true
             //   }
             // },
-            { //babel转换
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        [
-                            "@babel/preset-env",
-                            {
-
-                                useBuiltIns: "usage",
-
-                                corejs: {
-                                    version: 3
-                                },
-
-                                targets: {
-                                    chrome: "60",
-                                    firefox: "60",
-                                    ie: "9",
-                                    safari: "10",
-                                    edge: "17"
-                                }
-                            }
-                        ]
+            oneOf: [ //以下loader只会匹配一个
+                {
+                    test: /\.css$/,
+                    use: [...commonCssLoader]
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        ...commonCssLoader,
+                        "less-loader"
                     ]
+                },
+                {
+                    test: /\.(jpg|png|gif)$/,
+                    loader: "url-loader",
+                    options: {
+                        limit: 8 * 1024,
+                        esModule: false,
+                        name: "[hash:10].[ext]",
+                        outputPath: "imgs"
+                    }
+                },
+                {
+                    test: /\.html$/,
+                    loader: "html-loader"
+                },
+                {
+                    exclude: /\.(css|js|html|less|jpg|png|gif)$/,
+                    loader: "file-loader",
+                    options: {
+                        name: "[hash:10].[ext]",
+                        outputPath: "media"
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+
+                                    useBuiltIns: "usage",
+
+                                    corejs: {
+                                        version: 3
+                                    },
+
+                                    targets: {
+                                        chrome: "60",
+                                        firefox: "60",
+                                        ie: "9",
+                                        safari: "10",
+                                        edge: "17"
+                                    }
+                                }
+                            ]
+                        ]
+                    }
                 }
-            }
-        ]
+            ]
+        }]
     },
     plugins: [
         new HtmlWebpackPlugin({
