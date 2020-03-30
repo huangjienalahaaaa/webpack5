efefb62c5c58843ed0385f16d696757b806dec7a
hj <137612270@qq.com>
2020-03-30 16:51:23 +0800

    f

---
+++ webpack.dll.js
@@ -0,0 +1,30 @@
+/*
+    1. 使用dll技术,对某些库(指第三方库:如jquery,vue,react...)进行单独打包.我们这里使用jquery为例
+    2. 当你运行webpack时,默认查找的是webpack.config.js这个配置文件.而我们现在的需求是:需要运行webpack.dill.js文件:
+        所以我们运行指令要改,改成:webpack --config webpack.dill.js
+*/
+const {
+    resolve
+} = require('path');
+
+
+//因为下面要用到webpack自带的一个插件DllPlugin(作用:建立依赖关系,告诉webpack:将来打包的时候不需要打包jquery了),所以这里直接引入webpack
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        jquery: ['jquery'] //1. 前面这个jquery表示:打包生成的name叫做jquery. 2.后面这个jquery表示:要打包的这个库叫做jquery.因为这里是数组,那么将来与jquery类似的库,都可以放入这个数组里面,都可以进行打包
+    },
+    output: {
+        filename: '[name].js', //[name]就是上面entry中中前面那个jquery,
+        path: resolve(__dirname, 'dill'),
+        library: '[name]_[hash]', //表示:我们打包的库,里面向外暴露出去的内容叫什么名字
+    },
+    plugins: [
+        new webpack.DllPlugin({ //这个插件的是帮助我们生成一个manifest.json文件,这个文件呢,提供和jquery的映射关系,那么将来通过映射我们就知道,jquery这个库呢,原来不需要打包
+            name: '[name]_[hash]', //映射库的暴露的内容名称,跟上面的output中的library一致,这样才能建立关系.
+            path: resolve(__dirname, 'dill/manifest.json')
+        })
+    ],
+    mode: 'production'
+}
