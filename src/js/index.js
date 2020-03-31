bf50f6961e48b425bbe4e5e6b2e44991a5fbfa96
hj <137612270@qq.com>
2020-03-31 11:18:15 +0800

    entry

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import add from './add';
+import count from './count';
+
+console.log('index.js文件被加载了~!')
+console.log(add(2, 1))
+console.log(count(2, 1))
--- webpack.config.js
+++ webpack.config.js
@@ -1,156 +1,74 @@
 /* webpack配置详解
 ----------第一节------------
-可以去webpack中文官网->文档->配置中看(https://www.webpackjs.com/configuration/)
+可以去webpack中文官网(因为对着文档开发,能接触到最新的东西)->文档->配置中看(https://www.webpackjs.com/configuration/)
+
+----------第二节------------
+webpack详细配置之entry(入口文件):
+
+* 1.string:
+
+    1.形式:  "./src/js/index.js"
+
+    2.特点:  所有文件全都打包生成一个chunk,输出一个bundle文件.此时chunk的默认名称叫做main.js(除非在output/filename设定输出名称)
+
+* 2.array:
+
+    1.形式:  ["./src/js/index.js", "./src/js/add.js"]
+
+    2.特点:  多入口,所有入口文件最终只会生成一个chunk文件,输出一个bundle文件.此时chunk的默认名称叫做main.js(如这个实例中,是将index.js和add.js打包成一个chunk,名字叫做main.js)
+
+    3.作用: 这种形式只有一种用法.就是之前做HRM功能的时候,html内容修改后不能热更新了,所以使用这种方法.
+
+* 3.object:
+
+    1.形式( key - value ):  
+            {
+                index: "./src/js/index.js",
+                add: "./src/js/add.js"
+            },
+
+    2.特点: 多入口,有几个文件就形成几个chunk,输出几个bundle文件.此时chunk的名称就是key值.
+
+
+* 通常情况下,我们第一种和第三种用的比较多,第二种是特殊情况下才会使用到的.
+
+4.特殊用法: 
+     1.形式( key - value ):  
+        { 
+            index: ["./src/js/index.js", "./src/js/count.js"], //这里是数组
+            add: "./src/js/add.js"
+        }
+
+    2.特点: 对于数组里的这2个文件,跟第二点是相同的,如这个实例中,是将index.js和count.js打包成一个chunk,名字叫做index.js,而add.js会单独打包,名字叫做add.js
+    3.这个用法我们在哪里看过呢?在之前的dill时候的webpack.dill.js文件里面:
+        entry{
+            jquery:['jquery'], //这里
+            // react:['react','react-dom','react-router-dom'] //所以以后我们可以使用这种方式,来打包react全家桶到同一个文件中
+        }
  */
 const {
     resolve
 } = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-
-const webpack = require("webpack");
-const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
-
-process.env.NODE_ENV = "production";
-
-const commonCssLoader = [
-    MiniCssExtractPlugin.loader,
-    "css-loader",
-    {
-        loader: "postcss-loader",
-        options: {
-            ident: "postcss",
-            plugins: () => [require("postcss-preset-env")()]
-        }
-    }
-];
 
 module.exports = {
-    entry: ["./src/js/index.js"],
+    // entry: "./src/js/index.js", //1.string类型
+    // entry: ["./src/js/index.js", "./src/js/add.js"], //2.array类型
+    // entry: { //3.object类型
+    //     index: "./src/js/index.js",
+    //     add: "./src/js/add.js"
+    // },
+    entry: { //4.特熟用法
+        index: ["./src/js/index.js", "./src/js/count.js"], //这里是数组
+        add: "./src/js/add.js"
+    },
     output: {
-        filename: "js/[name].[contenthash:10].js",
+        filename: "[name].js", //使用默认名称
         path: resolve(__dirname, "build")
     },
-    module: {
-        rules: [{
-            // { //eslint语法检查
-            //   test: /\.js$/,
-            //   exclude: /node_modules/,
-            //   loader: "eslint-loader",
-            //   enforce: 'pre',
-            //   options: {
-            //     fix: true
-            //   }
-            // },
-            oneOf: [{
-                    test: /\.css$/,
-                    use: [...commonCssLoader]
-                },
-                {
-                    test: /\.less$/,
-                    use: [...commonCssLoader, "less-loader"]
-                },
-                {
-                    test: /\.(jpg|png|gif)$/,
-                    loader: "url-loader",
-                    options: {
-                        limit: 8 * 1024,
-                        esModule: false,
-                        name: "[hash:10].[ext]",
-                        outputPath: "imgs"
-                    }
-                },
-                {
-                    test: /\.html$/,
-                    loader: "html-loader"
-                },
-                {
-                    exclude: /\.(css|js|html|less|jpg|png|gif)$/,
-                    loader: "file-loader",
-                    options: {
-                        name: "[hash:10].[ext]",
-                        outputPath: "media"
-                    }
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: [{
-
-                            loader: "thread-loader",
-                            options: {
-                                workers: 2
-                            }
-                        },
-                        {
-                            loader: "babel-loader",
-                            options: {
-                                presets: [
-                                    [
-                                        "@babel/preset-env",
-                                        {
-                                            useBuiltIns: "usage",
-
-                                            corejs: {
-                                                version: 3
-                                            },
-
-                                            targets: {
-                                                chrome: "60",
-                                                firefox: "60",
-                                                ie: "9",
-                                                safari: "10",
-                                                edge: "17"
-                                            }
-                                        }
-                                    ]
-                                ],
-                                cacheDirectory: true
-                            }
-                        }
-                    ]
-                }
-            ]
-        }]
-    },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            minify: {
-                collapseWhitespace: true,
-                removeComments: true
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/build.[contenthash:10].css"
-        }),
-        new OptimizeCssAssetsPlugin(),
-        new webpack.DllReferencePlugin({
-            manifest: resolve(__dirname, "dll/manifest.json")
-        }),
-        new AddAssetHtmlPlugin({
-            filepath: resolve(__dirname, 'dll/jquery.js')
-        })
+        new HtmlWebpackPlugin({})
     ],
-    optimization: {
-        splitChunks: {
-            chunks: "all"
-        }
-    },
-
-    mode: "production",
-    externals: {
-        jquery: "jQuery"
-    },
-
-    devServer: {
-        contentBase: resolve(__dirname, "build"),
-        compress: true,
-        open: true,
-        port: 3000,
-        hot: true
-    },
-    devtool: "source-map"
+    mode: "development"
 };
