c58fabe40a8ef00196dcb7b80f9baca9a5676bfb
hj <137612270@qq.com>
2020-04-01 11:34:46 +0800

    a

--- webpack.config.js
+++ webpack.config.js
@@ -33,14 +33,34 @@ module.exports = {
             //忽略文件
             ignored: /node_modules/
         },
-        //启动gzip压缩
+        // 启动gzip压缩
         compress: true,
-        //端口号
+        // 端口号
         port: 5000,
         // 域名
         host: "localhost",
         // 自动打开浏览器
-        open: true
-        //开启HRM功能
+        open: true,
+        // 开启HRM功能
+        hot: true,
+        //　不要显示启动服务器日志信息
+        clientLogLevel: 'none',
+        //除了一些基本启动信息之外，其他内容都不显示
+        quiet: true,
+        //如果出错了，不要全屏显示～
+        overlay: false,
+        // 服务器代理－>解决开发环境跨域问题
+        proxy: {
+            // 一旦devServer(5000)服务器接受到/api/xxx的请求，就会把请求转发到另外一个服务器(3000)
+            'api': {
+                target: 'http://localhost:3000',
+                //发送请求时，请求路径重写：将 /api/xxx--->xxx (去掉/api)
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+
+
     }
 };
